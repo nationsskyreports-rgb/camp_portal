91ebf75a1ccec59748fa780a1025afb60d938b2c
nationsskyreports-rgb <nations.sky.reports@gmail.com>
2026-06-10 13:16:07 +0300

    Update page-campaigns.js

--- js/page-campaigns.js
+++ js/page-campaigns.js
@@ -56,7 +56,7 @@ function copyFormLinkBtn(campaignId) {
 }
 
 function copyIntakeLink(campaignId) {
-  var base = window.location.origin + window.location.pathname.replace(/[^/]*$/, '');
+  var base = window.location.origin + '/';
   var url  = base + 'intake.html?c=' + campaignId;
   navigator.clipboard.writeText(url).then(function() {
     toast('Form link copied!', 'success');
